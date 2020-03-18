490ec2069a4c62fb24283353caa185759f8db0ec
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-03-18 15:51:23 -0700

    basic set up and wireframe

--- src/App.js
+++ src/App.js
@@ -16,3 +16,15 @@ function App() {
 }
 
 export default App;
+
+//const [state, setState] = useState(0)
+
+// Your API key for rsachdeva@ucdavis.edu is:
+
+// XdxdNOQsBkUi5YJkbjNIlVtnPD75yGFAePvauVhr
+// You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
+
+// https://api.nasa.gov/planetary/apod?api_key=XdxdNOQsBkUi5YJkbjNIlVtnPD75yGFAePvauVhr
+
+axios
+.get('https://api.nasa.gov/planetary/apod?api_key=XdxdNOQsBkUi5YJkbjNIlVtnPD75yGFAePvauVhr')
